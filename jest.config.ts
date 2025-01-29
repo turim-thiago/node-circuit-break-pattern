de19e9c9b99a2a7a31ed932876e3766ded106d67
Thiago Turim <turimthiago@gmail.com>
2025-01-29 14:12:20 -0300

    chore init project

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 export default {
+	clearMocks: true,
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	roots: ['<rootDir>/tests'],
 	testMatch: ['**/tests/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	resetMocks: true,
   };
   
